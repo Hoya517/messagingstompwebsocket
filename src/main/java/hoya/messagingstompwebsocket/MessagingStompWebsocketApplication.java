ab47d2b063b380e23b925f0a582c0e8a40669ab5
Kim Juho <sv05159@naver.com>
2021-10-26 22:25:43 +0900

    Make the Application Executable

--- src/main/java/hoya/messagingstompwebsocket/MessagingStompWebsocketApplication.java
+++ src/main/java/hoya/messagingstompwebsocket/MessagingStompWebsocketApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MessagingstompwebsocketApplication {
+public class MessagingStompWebsocketApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MessagingstompwebsocketApplication.class, args);
+		SpringApplication.run(MessagingStompWebsocketApplication.class, args);
 	}
 
 }
